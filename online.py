6328229ea462dc8f74481effae81222f3dee35cc
steve <steve@wav-tst-002.waverley>
2022-05-20 10:04:57 +0100

    comment

---
+++ online.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+
+)
+
+x = st.slider('x') +10 # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+chart_data = pd.DataFrame(
+     np.random.randn(x, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+
+map_data = pd.DataFrame(
+    np.random.randn(x * 10, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+
+st.map(map_data)
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+st.write("Here's our first attempt at using data to create a table:")
+if st.checkbox('Show dataframe'):
+  map_data
+
+  st.write(pd.DataFrame({
+      'first column': [1, 2, 3, 4],
+      'second column': [10, 20, 30, 40]
+  }))
